6811f38ac8012515f186e8d61de66d9bd67a8818
Michi <muffinmichi@gmx.de>
2016-04-20 09:30:37 +0200

    Menü hinzugefügt

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("testScene");
+    }
+
+}
