a4569341d1280c3f4da1218e1fb65cae9c27a36c
Michi <muffinmichi@gmx.de>
2016-05-19 11:46:51 +0200

    Kamera Steuerung ist nun möglich. Die Main Kamera kann mit A und D bewegt werden. Mit LEERTATSE kann die Kamera gewechselt werden auf eine Kamera die immer auf das Dorf zeigt. Diese Kamera kann mit W und S gesteuert werden.

---
+++ Assets/Scripts/GameLogic/CameraControll.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+
+    public GameObject m_mainCamera;
+    public GameObject m_villageCamera;
+
+    public float m_cameraSpeed = 5.0f;
+
+    //values for the position and rotation of the main camera
+    private float m_translationX1 = -3.0f;
+    private float m_translationY1 = 7.0f;
+    private float m_translationZ1 = 3.5f;
+
+    private float m_rotationX1 = 50.0f;
+    private float m_rotationY1 = 80.0f;
+    private float m_rotationZ1 = 0.0f;
+
+    //values for the position and rotation of the village camera
+    private float m_translationXVillage = 5.0f;
+    private float m_translationYVillage = 5.0f;
+    private float m_translationZVillage = -4.0f;
+
+    private float m_rotationXVillage = 50.0f;
+    private float m_rotationYVillage = 0.0f;
+    private float m_rotationZVillage = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_mainCamera.transform.Translate(m_translationX1, m_translationY1, m_translationZ1);
+        m_mainCamera.transform.Rotate(m_rotationX1, m_rotationY1, m_rotationZ1);
+        m_mainCamera.SetActive(true);
+
+        setVillageCamera();
+        m_villageCamera.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (m_mainCamera.activeSelf)
+            {
+                m_mainCamera.SetActive(false);
+                setVillageCamera();
+                m_villageCamera.SetActive(true);
+            }
+            else
+            {
+                m_mainCamera.SetActive(true);
+                m_villageCamera.SetActive(false);
+            }
+        }
+
+        if (m_mainCamera.activeSelf)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                //transform.Translate(new Vector3(-(m_cameraSpeed * Time.deltaTime), 0.0f, 0.0f));
+                m_mainCamera.transform.position += new Vector3(0.0f, 0.0f, m_cameraSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                //Vector3 currPos = m_mainCamera.transform.position;
+                //Vector3 toAdd = new Vector3(0.0f, 0.0f, -(m_cameraSpeed * Time.deltaTime));
+                //Vector3 toGoTo = currPos + toAdd;
+                //m_mainCamera.transform.position = Vector3.Lerp(currPos, toGoTo, Time.deltaTime * 50f);
+                m_mainCamera.transform.position += new Vector3(0.0f, 0.0f, -(m_cameraSpeed * Time.deltaTime));
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_villageCamera.transform.position += new Vector3(0.0f, 0.0f, m_cameraSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                m_villageCamera.transform.position += new Vector3(0.0f, 0.0f, -(m_cameraSpeed * Time.deltaTime));
+            }
+        }
+	}
+
+    private void setVillageCamera()
+    {
+        m_villageCamera.transform.position = new Vector3(m_translationXVillage, m_translationYVillage, m_translationZVillage);
+        m_villageCamera.transform.eulerAngles = new Vector3(m_rotationXVillage, m_rotationYVillage, m_rotationZVillage);
+    }
+}
