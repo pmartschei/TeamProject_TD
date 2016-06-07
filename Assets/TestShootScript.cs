d7a7910c3c8d8ddb42e3fb24b2ba679abc3b1178
pmartschei <philipp.martschei@gmx.net>
2016-06-07 09:52:54 +0200

    TestScripts

---
+++ Assets/TestShootScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestShootScript : MonoBehaviour {
+
+    public GameObject m_bullet;
+    public GameObject m_target;
+    private float m_Timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_Timer += Time.deltaTime;
+        if (m_Timer > 1)
+        {
+            GameObject obj=Instantiate(m_bullet);
+            TestForceToTargetScript s= obj.GetComponent<TestForceToTargetScript>();
+            s.target = m_target;
+            m_Timer = 0;
+        }
+	}
+}
